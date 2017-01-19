ced30fb9b20001a3700469425dcc41cce9f4d702
Victor Bairak <victor.bairak@sigma.software>
2017-01-19 11:11:57 +0200

    #1903 - Updates in External_Blogs class.

--- includes/class-external-blogs.php
+++ includes/class-external-blogs.php
@@ -18,30 +18,61 @@ namespace UB\External_Blogs;
  */
 class External_Blogs {
 
+	/**
+	 * Get blogs (among $domains) from remote networks(or multinetworks).
+	 *
+	 * @param array $domains
+	 *
+	 * @return array
+	 */
 	public function get_external_blogs( $domains = array() ) {
 
 		$blogs = array();
 		$remote_urls = $this->get_remote_urls();
 		foreach ( $remote_urls as $url ) {
 			if ( $endpoint = $this->get_remote_endpoint( $url ) ) {
-				$blogs = $this->request( $endpoint, $domains );
+				$blogs_response = $this->request( $endpoint, $domains );
+				foreach ( $blogs_response as $domain => $blog ) {
+					if ( $blog && empty( $blogs[ $domain ] ) ) {
+						$blogs[ $domain ] = $blog;
+					}
+				}
 			}
 		}
-
 
 		return $blogs;
 	}
 
+	/**
+	 * Make request to external endpoint for blogs data.
+	 *
+	 * @param string $endpoint
+	 * @param array  $domains
+	 *
+	 * @return array
+	 */
 	private function request( $endpoint = '', $domains = array() ) {
-		$domains = strtolower( implode( ',', $domains ) );
-		$url = $endpoint . $domains;
+
+		$blogs    = array();
+		$domains  = strtolower( implode( ',', $domains ) );
+		$url      = $endpoint . $domains;
 		$response = wp_remote_get( $url );
-		return array();
+
+		if ( ! is_wp_error( $response ) || wp_remote_retrieve_response_code( $response ) === 200 ) {
+			$blogs = (array) json_decode( $response['body'], true );
+		}
+
+		return $blogs;
 	}
 
+	/**
+	 * Get list of remote urls (one url per external network) that we should to check for blogs.
+	 *
+	 * @return array
+	 */
 	private function get_remote_urls() {
 
-		// TODO instead of using constant add Multinetwork option for that.
+		// TODO: instead of using constant add Multinetwork option for that.
 		if ( defined( 'EXTERNAL_NETWORK_URL' ) ) {
 			return (array) EXTERNAL_NETWORK_URL;
 		}
@@ -49,6 +80,13 @@ class External_Blogs {
 		return array();
 	}
 
+	/**
+	 * Get remote endpoint url.
+	 *
+	 * @param string $url
+	 *
+	 * @return bool|string
+	 */
 	private function get_remote_endpoint( $url = '' ) {
 
 		$endpoint = false;
